537187e96d3d7ab7dd494f35385c935cf5b67e55
bertkoomen <75787741+bertkoomen@users.noreply.github.com>
2021-10-07 16:27:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,36 @@ function toggleHamburgerMenu(){
 
 }
 
+// darkmode//
+
+// var colorSwitch = document.getElementById('input-color-switch');
+
+// colorSwitch.addEventListener('click', checkMode);
+
+// function checkMode(){
+//     console.log('checking..');
+
+//     if (colorSwitch.checked){
+//         console.log('dark on');
+//         darkModeOn();
+//     }
+//     else{ 
+//         console.log('dark off');
+//         darkModeOff();
+//     }
+// }
+
+// function darkModeOn(){
+//     document.body.classList.toggle("dark-mode")
+
+// }
+
+// function darkModeOff(){
+//     document.body.classList.toggle("dark-mode")
+    
+// }
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
